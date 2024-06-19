16b27b27bfbc31c4dde6b1f081ac6bc4d1bf563c
Sofia Berdichevska <sofiaxberdichevska@gmail.com>
2024-06-19 20:13:20 +0200

    feat: add function to populate display
    - Create `populateDisplay` function to handle button clicks and update display
    - Define `display` variable to target the calculator display element
    - Attach event listeners to buttons for display update functionality

--- index.js
+++ index.js
@@ -18,21 +18,37 @@ let firstNumber;
 let secondNumber;
 let operator;
 
-function operate(firstNumber, secondNumber, operator){
-  switch(operator){
-    case "+": 
-      add(firstNumber,secondNumber);
+function operate(firstNumber, secondNumber, operator) {
+  switch (operator) {
+    case "+":
+      add(firstNumber, secondNumber);
       break;
     case "-":
-      subtract(firstNumber,secondNumber);
+      subtract(firstNumber, secondNumber);
       break;
     case "*":
-      multiply(firstNumber,secondNumber);
+      multiply(firstNumber, secondNumber);
       break;
     case "/":
-      divide(firstNumber,secondNumber);
+      divide(firstNumber, secondNumber);
       break;
     default:
-      return "Invalid operator";  
+      return "Invalid operator";
   }
 }
+
+let display = document.querySelector(".display");
+let displayValue = "";
+
+let buttons = document.querySelectorAll("button");
+
+function populateDisplay() {
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      displayValue += e.target.textContent;
+      display.textContent = displayValue;
+    });
+  });
+}
+
+populateDisplay();
